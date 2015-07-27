c99713c76b1f1b7482dbb136abafee15ee24d0db
Gilbert Fine <gilbert.fine@gmail.com>
2015-07-27 23:40:40 +0800

    add two new functions: resourceDownloadUrl', authedResourceDownloadUrl'

--- Qiniu/URL.hs
+++ Qiniu/URL.hs
@@ -14,11 +14,24 @@ resourceDownloadUrl :: IsString s =>
     -> Bucket
     -> ResourceKey
     -> s
-resourceDownloadUrl m_domain bucket rkey = fromString $
+resourceDownloadUrl m_domain bucket rkey = resourceDownloadUrl' m_domain bucket rkey Nothing
+
+
+resourceDownloadUrl' :: IsString s =>
+    Maybe String        -- ^ domain
+    -> Bucket
+    -> ResourceKey
+    -> Maybe String     -- ^ optional query string
+    -> s
+resourceDownloadUrl' m_domain bucket rkey m_qs = fromString $
     concat
         [ "http://"
         , fromMaybe (unBucket bucket ++ ".qiniudn.com") m_domain
         , keyToUrlPath rkey
+        , case m_qs of
+            Nothing         -> ""
+            Just qs@('?':_) -> qs
+            Just qs         -> '?' : qs
         ]
 
 
@@ -31,5 +44,17 @@ authedResourceDownloadUrl :: IsString s =>
     -> ResourceKey
     -> s
 authedResourceDownloadUrl skey akey expiry m_domain bucket rkey =
+    authedResourceDownloadUrl' skey akey expiry m_domain bucket rkey Nothing
+
+authedResourceDownloadUrl' :: IsString s =>
+    SecretKey
+    -> AccessKey
+    -> UTCTime
+    -> Maybe String        -- ^ domain
+    -> Bucket
+    -> ResourceKey
+    -> Maybe String     -- ^ optional query string
+    -> s
+authedResourceDownloadUrl' skey akey expiry m_domain bucket rkey m_qs =
     fromString $
-        authedDownloadUrl skey akey expiry $ resourceDownloadUrl m_domain bucket rkey
+        authedDownloadUrl skey akey expiry $ resourceDownloadUrl' m_domain bucket rkey m_qs
