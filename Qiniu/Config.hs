1662a3b43ffbced95d4a9d50d78f61561ba12089
Gilbert Fine <gilbert.fine@gmail.com>
2017-08-17 11:15:12 +0800

    add new config field: path prefix

--- Qiniu/Config.hs
+++ Qiniu/Config.hs
@@ -11,10 +11,12 @@ import Qiniu.Types
 
 data QiniuConfig =
        QiniuConfig
-         { qiniuConfigSecretKey :: SecretKey
-         , qiniuConfigAccessKey :: AccessKey
-         , qiniuConfigBucket :: Bucket
-         , qiniuConfigDomain :: Maybe String
+         { qiniuConfigSecretKey  :: SecretKey
+         , qiniuConfigAccessKey  :: AccessKey
+         , qiniuConfigBucket     :: Bucket
+         , qiniuConfigDomain     :: Maybe String
+         , qiniuConfigPathPrefix :: String
+         -- ^ 把 resource key 当路径用时，总是加上这段路径前缀
          }
   deriving (Eq, Show)
 
@@ -29,6 +31,7 @@ instance FromJSON QiniuConfig where
             <*> (fmap (Bucket . fromString) $
                                         o .: "bucket" >>= check_non_empty_str)
             <*> ( fmap nullToMaybe $ o .:? "domain" )
+            <*> ( fmap (fromMaybe "") $ o .:? "path-prefix" )
         where
             check_non_empty_str s = if null s then mzero else return s
 
@@ -40,12 +43,13 @@ instance FromJSON QiniuConfig where
 -- | 包含一个公开 bucket, 一个私有 bucket 的设置
 data QiniuDualConfig =
        QiniuDualConfig
-         { qcDualSecretKey :: SecretKey
-         , qcDualAccessKey :: AccessKey
-         , qcDualPublicBucket :: Bucket
-         , qcDualPublicDomain :: Maybe String
+         { qcDualSecretKey     :: SecretKey
+         , qcDualAccessKey     :: AccessKey
+         , qcDualPublicBucket  :: Bucket
+         , qcDualPublicDomain  :: Maybe String
          , qcDualPrivateBucket :: Bucket
          , qcDualPrivateDomain :: Maybe String
+         , qcDualPathPrefix    :: String
          }
   deriving (Eq, Show)
 
@@ -63,6 +67,7 @@ instance FromJSON QiniuDualConfig where
             <*> (fmap (Bucket . fromString) $
                                         o .: "private-bucket" >>= check_non_empty_str)
             <*> ( fmap nullToMaybe $ o .:? "private-domain" )
+            <*> ( fmap (fromMaybe "") $ o .:? "path-prefix" )
         where
             check_non_empty_str s = if null s then mzero else return s
 
@@ -78,6 +83,7 @@ pubOfQiniuDualConfig qc = QiniuConfig
                             (qcDualAccessKey qc)
                             (qcDualPublicBucket qc)
                             (qcDualPublicDomain qc)
+                            (qcDualPathPrefix qc)
 -- }}}1
 
 
@@ -88,6 +94,7 @@ priOfQiniuDualConfig qc = QiniuConfig
                             (qcDualAccessKey qc)
                             (qcDualPrivateBucket qc)
                             (qcDualPrivateDomain qc)
+                            (qcDualPathPrefix qc)
 -- }}}1
 
 
